9cda0bdfc9094600cb7e89e4f573e0f97d92bba1
Valentina P <vale.pal06@gmail.com>
2026-05-01 01:26:45 -0600

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,9 @@ $sql = "SELECT
 FROM envios e
 JOIN clientes c ON e.id_cliente = c.id_cliente
 JOIN vehiculos v ON e.id_vehiculo = v.id_vehiculo
-JOIN rutas r ON e.id_ruta = r.id_ruta;";
+JOIN rutas r ON e.id_ruta = r.id_ruta
+WHERE e.estado = 'ENTREGADO';";
+
 
 $result = $conn->query($sql);
 
